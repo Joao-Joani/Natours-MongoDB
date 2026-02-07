46eacaa07027a0b4035d78fb4c2a9761f35fe07e
Joao Joani <jgabriel.jaoni@gmail.com>
2026-02-06 21:02:32 -0300

    Finalizando a API.

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const morgan = require('morgan');
+
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.status(200)
-  .json({ message: 'Olá, do lado do servidor!', app: 'Natours' });
+// 1) Middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware 👋');
+  next();
 });
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Aplicativo rodando na porta ${port}...`);
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
 });
+
+// 3) Rotas
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
+
+module.exports = app;
