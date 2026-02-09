5a8e513e34d906a0d8b15db5d982cb602ecc6424
Joao Joani <jgabriel.jaoni@gmail.com>
2026-02-09 15:14:51 -0300

    Utlizando MongoBD com Mongoose.

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
+  console.log('Olá do middleware 👋');
   next();
 });
 
